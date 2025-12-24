e6171ea5c0b630ca406be2085f05252a4117abf2
scorpion963 <jackson20062008@gmail.com>
2025-12-23 20:16:39 -0600

    Added customize user form, it's ugly though and doesn't do anything

---
+++ apps/telegram-client/src/features/sidebar/components/CustomizeUserSidebar/CustomizeUserForm.tsx
@@ -0,0 +1,128 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { user } from "db";
+import { Edit } from "lucide-react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const customizeUserFormSchema = z.object({
+  firstName: z.string().trim().min(1),
+  lastName: z.string().trim().min(1),
+  bio: z.string().trim().min(1),
+  username: z.string().trim(),
+});
+
+export default function CustomizeUserForm({
+  defaultFields,
+}: {
+  defaultFields: z.infer<typeof customizeUserFormSchema>;
+}) {
+  const form = useForm<z.infer<typeof customizeUserFormSchema>>({
+    resolver: zodResolver(customizeUserFormSchema),
+    defaultValues: {
+      bio: defaultFields.bio,
+      firstName: defaultFields.firstName,
+      lastName: defaultFields.lastName,
+      username: defaultFields.firstName,
+    },
+  });
+
+  function onSubmit() {}
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="w-full flex items-center justify-center">
+          <div className="size-32 rounded-full bg-gray-500 flex items-center justify-center">
+            <Edit className="size-16" />
+          </div>
+        </div>
+
+        <FormField
+          name="firstName"
+          control={form.control}
+          render={({ field }) => (
+            <>
+              <FormItem>
+                <FormLabel>First name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            </>
+          )}
+        />
+
+        <FormField
+          name="lastName"
+          control={form.control}
+          render={({ field }) => (
+            <>
+              <FormItem>
+                <FormLabel>Last name (optional)</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            </>
+          )}
+        />
+
+        <FormField
+          name="bio"
+          control={form.control}
+          render={({ field }) => (
+            <>
+              <FormItem>
+                <FormLabel>Bio</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            </>
+          )}
+        />
+
+        <div className="text-muted-foreground text-sm">
+          <div>Any details such as age, occupation or city.</div>
+          <div>Example 23 y.o. desginer from San Francisco</div>
+        </div>
+
+        <div>
+          {" "}
+          <div>Username</div>
+          <FormField
+            name="bio"
+            control={form.control}
+            render={({ field }) => (
+              <>
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              </>
+            )}
+          />
+          <p>
+            You can choose a username on cucumber. If you do, people will be
+            able to find you by this username.
+          </p>
+          <p>
+            You can use a-z, 0-9 and underscores. Minimum length is 5
+            characters. This link opens a chat with you:
+            https://t.me/dimasik23123
+          </p>
+        </div>
+      </form>
+    </Form>
+  );
+}
--- apps/telegram-client/src/features/sidebar/components/CustomizeUserSidebar/CustomizeUserSidebar.tsx
+++ apps/telegram-client/src/features/sidebar/components/CustomizeUserSidebar/CustomizeUserSidebar.tsx
@@ -2,12 +2,14 @@
 
 import { useSidebarRouterStore } from "@/components/SidebarRouter/providers/sidebar-routes-provider";
 import { Button } from "@/components/ui/button";
+import CustomizeUserForm from "./CustomizeUserForm";
 
 export default function CustomizeUserSidebar() {
   const { pop } = useSidebarRouterStore((state) => state);
   return (
     <div>
       <Button onClick={() => pop()}>back</Button>
+      <CustomizeUserForm defaultFields={{bio: "", firstName: "", lastName: "", username: ""}} />
     </div>
   );
 }
